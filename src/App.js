200db77b6d408141a5f240760017e983d09228c6
David Gonzalezn <drck@gmail.com>
2022-10-08 01:45:40 -0500

    Inicializa proyecto con firebas

--- src/App.js
+++ src/App.js
@@ -24,19 +24,30 @@ function App() {
 
 export default App;
 */
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import {TextField , Button } from '@mui/material';
 import Todo from './components/Todo';
+import { db } from './firebase.js';
+import { collection , query, orderBy , onSnapshot, addDoc,serverTimestamp} from 'firebase/firestore';
 import './App.css';
+const q=query(collection(db,'todos'),orderBy('timestamp','desc'));
 function App() {
-const [todos,setTodos]=useState([
-'Create Blockchain App',
-'Create a Youtube Tutorial'
-]);
+const [todos,setTodos]=useState([]);
 const [input, setInput]=useState('');
+useEffect(() => {
+onSnapshot(q,(snapshot)=>{
+setTodos(snapshot.docs.map(doc=>({
+id: doc.id,
+item: doc.data()
+})))
+})
+},[input]);
 const addTodo=(e)=>{
 e.preventDefault();
-setTodos([...todos,input]);
+addDoc(collection(db,'todos'),{
+todo:input,
+timestamp: serverTimestamp()
+})
 setInput('')
 };
 return (
@@ -48,7 +59,7 @@ onChange={e=>setInput(e.target.value)} />
 <Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
 </form>
 <ul>
-{todos.map(todo => <Todo todo={todo} />)}
+{todos.map(item=> <Todo key={item.id} arr={item} />)}
 </ul>
 </div>
 );
--- src/firebase.js
+++ src/firebase.js
@@ -7,3 +7,17 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAKrlsF1bhukeBxXNJ0xQit4Vrj3A9pXqo",
+  authDomain: "quetzalcoatl-de28b.firebaseapp.com",
+  projectId: "quetzalcoatl-de28b",
+  storageBucket: "quetzalcoatl-de28b.appspot.com",
+  messagingSenderId: "242820719201",
+  appId: "1:242820719201:web:184db2bb7e4ff7bb7a4020"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+export { db }
